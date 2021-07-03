add81f16b24c66c835ecafc79009252d785d06cb
Vitaliy Mandzyak <vmandzyak@seiitra.com>
2021-07-03 10:27:11 +0300

    Added controllers with mocked data

---
+++ Hackaton.WebApi/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackaton.WebApi.Controllers
+{
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{userId}/videos")]
+        public IActionResult Videos(int userId, string priority = "desc")
+        {
+            var arrayResultDesc = new[]
+            {
+                new { Video = "Video 5", Priority = "high" },
+                new { Video = "Video 6", Priority = "high" },
+                new { Video = "Video 7", Priority = "high" },
+                new { Video = "Video 8", Priority = "medium" }
+            };
+
+            var arrayResultAsc = new[]
+            {
+                new { Video = "Video 8", Priority = "medium" },
+                new { Video = "Video 5", Priority = "high" },
+                new { Video = "Video 6", Priority = "high" },
+                new { Video = "Video 7", Priority = "high" }
+            };
+
+            return Ok(priority == "asc" ? arrayResultAsc : arrayResultDesc);
+        }
+
+        [HttpGet("{userId}/videos/{videoId}")]
+        public IActionResult Paths(int userId, int videoId, string priority = "desc")
+        {
+            var arrayResultDesc = new[]
+            {
+                new { Path = $"users/{userId}/groups/5/flows/4/videos/{videoId}", Priority = "high" },
+                new { Path = $"users/{userId}/flows/4/videos/{videoId}", Priority = "medium" },
+                new { Path = $"users/{userId}/videos/{videoId}", Priority = "low" }
+            };
+
+            var arrayResultAsc = new[]
+            {
+                new { Path = $"users/{userId}/videos/{videoId}", Priority = "low" },
+                new { Path = $"users/{userId}/flows/4/videos/{videoId}", Priority = "medium" },
+                new { Path = $"users/{userId}/groups/5/flows/4/videos/{videoId}", Priority = "high" }
+            };
+
+            return Ok(priority == "asc" ? arrayResultAsc : arrayResultDesc);
+        }
+    }
+}
